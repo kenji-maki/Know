85882bf961db3613195d842730ba0065d7acbdff
名前 <>
2020-12-08 12:09:20 +0000

    1208

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   helper_method :current_user, :logged_in?
+  before_action :login_required
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -11,4 +12,9 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !current_user.nil?
   end
+  
+  def login_required
+    redirect_to login_url unless current_user
+  end
+  
 end
